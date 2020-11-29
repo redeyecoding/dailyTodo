71dc63afec5897e8565cc3cecd1aa81100f2bb59
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-29 12:59:20 -0500

    started for for route that registers users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,33 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/users');
+
+const { check, validationResult } = require('express-validator');
 
 
-// GET 
+// POST /api/users
 // desc Register user
 // @access Public
-router.get('/', (req, res) => {
+
     // Registering the user will return a unique token.
     // If information user enters already exists,
     // tell them error and say login
     // ELSE you create a user account for them
+router.post('/', [
+    check('email', 'Email Address is required')
+        .isEmail(),
+    check('name','Your name is required')
+        .not()
+        .isEmpty(),
+    check('password', 'Password must be 6 characters or more')
+        .isLength({ min: 6 })
+    ], async (req, res) => {
 
-    
-    // check for user in database
+        // Validate user information
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        };
 
-    
-    // Validate user information
+        const {
+            email,
+            name,
+            password
+        } = req.body;        
+        
+        try {
+            // check for user in database ( via email )
+            let user = await User.findOne({ email: email });
+            if (user) {
+                res.status(400).json('That user already exists')
+            };
 
-    
-    // build user Object from request
+            // build user Object from request
+            user = new User({
+                name: name,
+                password: password,
+                email: email
+            });
 
-    
-    // hash their password 
+            
+            console.log(user)
 
 
-    // Assemble Token
+            // hash their password 
 
+            res.json('SUCCESS')
+            // Assemble Token
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ msg: 'Server Error' });
+        };
 
-    res.send('users api is working');
 });
 
 
