c753da1db58aae02a5c065b403c7751c51f6e830
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-29 12:58:52 -0500

    added routes to server and added json parser

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const config = require('config');
 const mongoose = require('mongoose');
 const connectToDatabase = require('./config/db');
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 // Connect to Database
 connectToDatabase();
 
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/todo-list', require('./routes/api/todoList'));
 
 
 app.listen(port, () => console.log(`DailyTodo API IS NOW RUNNING ON PORT: ${port}`));
