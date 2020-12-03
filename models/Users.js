fc3e5a854396487699df74032f26e2c4606c97ff
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-02 22:37:41 -0500

    rename file to capitialized

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    }
+});
+
+module.exports = User = mongoose.model('user', userSchema);
