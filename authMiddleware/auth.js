ddcccee50c8fa18c91e80aa20d60b828dc2e8c32
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-02 22:24:03 -0500

    started middleware code

--- authMiddleware/auth.js
+++ authMiddleware/auth.js
@@ -1,1 +1,12 @@
 const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+
+const auth = (req, res, next) => {
+    console.log(req.body)
+    res.send('PROTECTED ROUTE')
+
+    next();
+};
+
+module.exports = auth;
