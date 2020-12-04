b5c20ced5058d10263524145cb395867c01fc800
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-04 18:58:14 -0500

    added some code for protected routes

--- authMiddleware/auth.js
+++ authMiddleware/auth.js
@@ -1,27 +1,33 @@
 const jwt = require('jsonwebtoken');
 const express = require('express');
-const router = express.Router();
-
+const bcrypt = require('bcrypt');
 
 const auth = (req, res, next) => {
-            // Pull token from request
+    // Pull password request
+    
+    const xAuthToken = req.headers['x-auth-token'];
+    // If no token respond with "Unauthorized" 
+    if (!xAuthToken){
+        return res.status(401).json({ msg: 'Access Denied, No Token' });
+    };
+    
+    try {
+        const decryptedToken = jwt.verify(xAuthToken, process.env.TOKEN_SECRET);
+    
+        if (!decryptedToken) {
+            return res.status(401).json({ msg: 'Invalid Token' });
+        };
 
-
-
-        // validate token
-            // If no token respond with "Unauthorized"
-
-
-
-        // If token but invalid, respond with "Invalid Token"
-
-
-
+        req.user = decryptedToken.user;
+        console.log(req.user)
+        next();
         
-        // If valid Token, modify add
-    res.send('PROTECTED ROUTE')
 
-    next();
+    } catch (err) {
+        console.error(err.message);
+        res.status(401).send('Access Denied');
+    }
+
 };
 
 module.exports = auth;
