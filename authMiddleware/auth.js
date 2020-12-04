e36d9195cbcdcb4e3a90b68105884de8f85aae8e
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-04 17:53:13 -0500

    started code for protected routes

--- authMiddleware/auth.js
+++ authMiddleware/auth.js
@@ -2,8 +2,23 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 
+
 const auth = (req, res, next) => {
-    console.log(req.body)
+            // Pull token from request
+
+
+
+        // validate token
+            // If no token respond with "Unauthorized"
+
+
+
+        // If token but invalid, respond with "Invalid Token"
+
+
+
+        
+        // If valid Token, modify add
     res.send('PROTECTED ROUTE')
 
     next();
