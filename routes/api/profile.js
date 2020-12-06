77984371b7b5f081fe99334902cf814ee458ad6f
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-06 12:19:48 -0500

    Added code to update existing profile and create new profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const auth = require('../../authMiddleware/auth');
 const { check, validationResult } = require('express-validator');
 const { TokenExpiredError } = require('jsonwebtoken');
-const User = require('../../models/Profile');
-const { findById } = require('../../models/Profile');
-
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
 
 // GET /api/profile
 // get all user profies
@@ -23,8 +22,22 @@ router.get('/', (req, res) => {
 // GET /api/profile/user/:id
 // Get user profile via ID
 // @access Private
-router.get('/user/:id', auth, (req, res) => {
-    res.json(req.user)
+router.get('/user/:id', auth, async(req, res) => {
+    try {    // Validate user's ID
+    const profile = await Profile.findOne({ id: req.params.id });
+    
+    if (!profile) {
+        return res.status(400).json({ msg: 'No profile for that user'});
+    };
+
+
+} catch(err) {
+    if (err.kind === 'ObjectId') {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    };
+}
+
 });
 
 
@@ -43,15 +56,41 @@ check('name', 'Your name is required')
         return res.status(400).json({ errors: errors.array() });
     };
 
+
     try {    // Validate user's ID
-        const user = await User.findOne({ _id: req.params.id });
-        console.log(user)
-        if (!user) {
-            return res.status(400).json({ msg: 'Unauthorized'})
+        let profile = await Profile.findOne({ user: req.params.id });
+        const user = await User.findById(req.user.id).select('-password');
+
+        const {
+            name,
+            bio
+        } = req.body;
+
+        // update existing profile
+        if (profile) {
+            profile = await Profile.findOneAndUpdate(
+                req.user.id, 
+                { $set: { 
+                    name: name, 
+                    bio: bio, 
+                    avatar: user.avatar, 
+                    date: Date.now() } },
+                {new: true})
+            return res.json(profile);
         };
 
-        console.log(user)
-        res.send(req.user)
+        // Create new Profile
+        profile = new Profile({
+            name: name,
+            bio: bio,
+            user: req.user.id,
+            avatar: user.avatar
+        });
+
+        
+
+        profile.save();
+        res.json(profile);
 
     } catch(err) {
         if (err.kind === 'ObjectId') {
