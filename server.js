b265d33921669f128bc4125caa9f36490c6be0e2
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-07 22:55:09 -0500

    small adjustment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectToDatabase();
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/todo-list', require('./routes/api/todoList'));
+app.use('/api/todo-list', require('./routes/api/todo'));
 
 
 app.listen(port, () => console.log(`DailyTodo API IS NOW RUNNING ON PORT: ${port}`));
