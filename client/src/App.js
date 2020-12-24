475027bb076b9c01dd787bf353e1c0a25e1435cc
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-24 16:47:50 -0500

    added header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Layout/Header/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>DAILY_TODO_APPS</h1>
     </div>
   );
