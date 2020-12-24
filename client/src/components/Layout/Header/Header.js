fc874800425083e93e21a1a97bcc913f7550b98d
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-24 16:47:21 -0500

    added header component

---
+++ client/src/components/Layout/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../../UI/Nav/Nav';
+import classes from './Header.css';
+
+const Header = () => {
+    return (
+        <div className='main-header'>
+            HEADER
+            <Nav />
+        </div>
+    )
+};
+
+export default Header;
