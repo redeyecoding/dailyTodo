9f1dabf62351ffe93ad20317b4d46a5154f15e5a
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-25 14:15:58 -0500

    refactered login card

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../Layout/Card/Card';
 import Header from '../Layout/Header/Header';
-import LoginInput from '../UI/LoginInput/LoginInput';
+import './Auth.css';
+import Button from '../UI/Button/Button';
+
 
 const Auth = () => {
+    const [ userEmail, setUserEmail ] = useState('');
+
+    const setUpLoginInformation = () => {
+        setUserEmail(userEmail)
+    }
     return  (
         <>
             <Header />
-            <Card>
-                LOGIN PLEASE
-                <LoginInput />
-            </Card>   
+            <section className='form_login-container'>
+                <Card>
+                    <form onSubmit={ 'D'}> 
+                        <div className='form_login-Input'>
+                            <label>Username</label>
+                            <input 
+                                placeholder='Email' 
+                                value={ userEmail }
+                                type='email' />
+                        </div>
+                        <div  className='form_login-Input'>
+                            <label>Password</label>
+                            <input 
+                                onChange={ setUpLoginInformation }
+                                placeholder='Password' 
+                                value={ userEmail }
+                                type='password' />
+                        </div>
+                        <Button 
+                            clicked={ () => '' }
+                            buttonType='submit'>SUBMIT</Button>
+                    </form>
+                </Card>
+            </section>    
         </>
 
     )
