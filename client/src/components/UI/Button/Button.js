7b8cf586ec9c2126b4ee37ed1caadb505505ac08
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-25 14:16:57 -0500

    Added props.children to needed areas

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Button = props => {
     return (
-        <button>{ props.children }</button>
+        <button 
+            onClick={ props.clicked }
+            type={ props.buttonType }>{ props.children }</button>
     )
 };
 
