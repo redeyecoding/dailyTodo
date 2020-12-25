d134e2c788a8b27878d78ca3fda5f7060955689a
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-24 19:05:18 -0500

    added input component for login and Registration page

---
+++ client/src/components/UI/LoginInput/LoginInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LoginInput = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <label>FIRST NAME</label>
+                    <input type='text'></input>        
+                </div>
+            </section>
+            
+        </>
+    )
+};
+
+export default LoginInput;
