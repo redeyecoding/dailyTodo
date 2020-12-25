ed785d02313fbc90d4f1b2b8bf44420625b78d2d
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-24 19:06:01 -0500

    added nav component

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Nav from '../../UI/Nav/Nav';
+import Card from '../Card/Card';
 import classes from './Header.css';
 
 const Header = () => {
     return (
         <div className='main-header'>
             HEADER
-            <Nav />
+            <Nav />            
         </div>
     )
 };
