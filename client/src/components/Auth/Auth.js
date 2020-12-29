ee4bc40bae364e31553300e23b7ac908fc578efd
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-28 22:29:51 -0500

    added some classes with corresponding css code

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -14,19 +14,23 @@ const Auth = () => {
     return  (
         <>
             <Header />
+            
             <section className='form_login-container'>
                 <Card>
-                    <form onSubmit={ 'D'}> 
+                    <form className="form_login-form" onSubmit={ 'D'}> 
+                    <span>PLEASE LOGIN</span>
                         <div className='form_login-Input'>
-                            <label>Username</label>
+                            <label class="form_login--labeling">Username</label>
                             <input 
+                                className="form_login--input"
                                 placeholder='Email' 
                                 value={ userEmail }
                                 type='email' />
                         </div>
                         <div  className='form_login-Input'>
-                            <label>Password</label>
+                            <label className="form_login--labeling">Password</label>
                             <input 
+                                className="form_login--input"
                                 onChange={ setUpLoginInformation }
                                 placeholder='Password' 
                                 value={ userEmail }
