606176c587c5285351c91e7fd0c4dab3fa488805
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-30 16:02:33 -0500

    imported modules from react-router-dom

--- client/src/components/UI/Nav/Nav.js
+++ client/src/components/UI/Nav/Nav.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import './Nav.css';
+import { NavLink } from 'react-router-dom';
 
-const Nav = prop => {
+
+const Nav = props => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    My Profile
+        <nav className="head-main-menu">
+            <ul className="head-main-menu_items">
+                <li className="head-main-menu_label">
+                    <NavLink
+                        className="nav-link"
+                        to="/">Home</ NavLink>
                 </li>
-                <li>
-                    My Lists
+
+                <li className="head-main-menu_label">
+                    <NavLink 
+                        className="nav-link"
+                        to="/profile">Profile</ NavLink>
+                </li>
+
+                <li className="head-main-menu_label">
+                    <NavLink 
+                        className="nav-link user-lists"
+                        to="/my-lists">My Lists</ NavLink>
                 </li>
             </ul>
         </nav>
