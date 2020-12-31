7bbc43525d2a6b5b932d800753dd758ba33287a2
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-30 19:51:51 -0500

    added some more code

--- client/src/components/UI/Nav/Nav.js
+++ client/src/components/UI/Nav/Nav.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
+import Logo from '../../Layout/Logo/Logo';
+
 
 
 const Nav = props => {
+    // Right code for having two separate headers.
     return (
-        <nav className="head-main-menu">
-            <ul className="head-main-menu_items">
-                <li className="head-main-menu_label">
-                    <NavLink
-                        className="nav-link"
-                        to="/">Home</ NavLink>
-                </li>
+        <>           
+            <Logo />
+            <nav className="head-main-menu header-area">
+                <ul className="head-main-menu_items">
+                    <li className="head-main-menu_label">
+                        <NavLink
+                            className="nav-link"
+                            to="/">Home</ NavLink>
+                    </li>
 
-                <li className="head-main-menu_label">
-                    <NavLink 
-                        className="nav-link"
-                        to="/profile">Profile</ NavLink>
-                </li>
+                    <li className="head-main-menu_label">
+                        <NavLink 
+                            className="nav-link"
+                            to="/profile">Profile</ NavLink>
+                    </li>
 
-                <li className="head-main-menu_label">
-                    <NavLink 
-                        className="nav-link user-lists"
-                        to="/my-lists">My Lists</ NavLink>
-                </li>
-            </ul>
-        </nav>
+                    <li className="head-main-menu_label">
+                        <NavLink 
+                            className="nav-link user-lists"
+                            to="/my-lists">My Lists</ NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
+
     )
 };
 
