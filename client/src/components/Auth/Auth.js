9fe25382b22f74d677e964b7ec943d95b9477e8b
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-30 19:52:57 -0500

    added some more code

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -17,39 +17,42 @@ const Auth = () => {
     }
     return  (
         <>
-            <Header />            
-            <section className='form_login-container'>
-                <Card>
-                    <form className="form_login-form" onSubmit={ 'D'}> 
-                    <span className="form_login-form_title">PLEASE LOGIN</span>
-                        <div className='form_login-Input'>
-                            <label class="form_login--labeling line--up">Username</label>
-                            <input 
-                                onChange={ event => setUserEmailHandler(event.target.value) }
-                                className="form_login--input line--up"
-                                placeholder='Email' 
-                                value={ userEmail }
-                                type='email' />
-                        </div>
-                        <div  className='form_login-Input'>
-                            <label className="form_login--labeling line--up">Password</label>
-                            <input 
-                                className="form_login--input line--up"
-                                onChange={ event => setUserPasswordHandler(event.target.value) }
-                                placeholder='Password' 
-                                value={ userPassword }
-                                type='password' />
-                        </div>
-                        <div className="form_login--submit">
-                            <button
-                                className="form_login--submit-btn"
-                                clicked={ () => '' }
-                                type='submit'>LOGIN</button>
-                        </div>
+            <Header /> 
+            <main className="login-container">
+                <section className='form_login-container'>
+                    <Card>
+                        <form className="form_login-form" onSubmit={ 'D'}> 
+                        <span className="form_login-form_title">PLEASE LOGIN</span>
+                            <div className='form_login-Input'>
+                                <label class="form_login--labeling line--up">Username</label>
+                                <input 
+                                    onChange={ event => setUserEmailHandler(event.target.value) }
+                                    className="form_login--input line--up"
+                                    placeholder='Email' 
+                                    value={ userEmail }
+                                    type='email' />
+                            </div>
+                            <div  className='form_login-Input'>
+                                <label className="form_login--labeling line--up">Password</label>
+                                <input 
+                                    className="form_login--input line--up"
+                                    onChange={ event => setUserPasswordHandler(event.target.value) }
+                                    placeholder='Password' 
+                                    value={ userPassword }
+                                    type='password' />
+                            </div>
+                            <div className="form_login--submit">
+                                <button
+                                    className="form_login--submit-btn"
+                                    clicked={ () => '' }
+                                    type='submit'>LOGIN</button>
+                            </div>
 
-                    </form>
-                </Card>
-            </section>    
+                        </form>
+                    </Card>
+                </section>                
+            </main>           
+            
         </>
 
     )
