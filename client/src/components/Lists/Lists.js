76733423cc5a096b0b5259ba9154f7f1b10ee4e3
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-31 15:05:45 -0500

    intial code for Lists page

---
+++ client/src/components/Lists/Lists.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../Layout/Header/Header';
+
+
+const Lists = props => {
+    return (
+        <>
+            <Header />
+            <main>
+                <section>
+                    Lists SECTION
+                </section>
+            </main>      
+        </>
+
+    )
+};
+
+export default Lists;
