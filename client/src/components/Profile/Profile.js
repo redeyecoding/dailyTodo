d526f5fa8b69ec9827ceb94e0369cb08ce52a21d
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-31 15:15:32 -0500

    intial code for Profile page

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../Layout/Header/Header';
+
+
+const Profile = props => {
+    return (
+        <>
+            <Header />
+            <main>
+                <section>
+                    PROFILE SECTION
+                </section>
+            </main>      
+        </>
+
+    )
+};
+
+export default Profile;
