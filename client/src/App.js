56ca04fd58e4a8ee17798e198975daa49d314e1a
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-01 18:00:09 -0500

    small adjustment

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './components/Auth/Login/Login';
 import Register from './components/Auth/Register/Register';
-
 import { Route, Switch } from 'react-router-dom';
 import Profile from './components/Profile/Profile';
 import Lists from './components/Lists/Lists';
+import Layout from './components/Layout/Layout';
+
 
 function App() {
   return (
