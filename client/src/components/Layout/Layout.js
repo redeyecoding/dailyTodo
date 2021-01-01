b4e9145903c6c024445f21f9990435bfc6b87ac4
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-01 18:07:12 -0500

    moved the folder to different location

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auth from '../../components/Auth/Auth';
+
+const Layout = () => {
+    return (
+        <>
+            <Auth />
+        </>
+    )
+};
+
+export default Layout;
