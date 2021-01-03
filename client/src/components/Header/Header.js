affa38455e9833421d3d6f62f40c8965b6d4cc27
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-03 12:46:31 -0500

    added log component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Nav from '../UI/Nav/Nav';
 import Card from '../UI/Card/Card';
 import classes from './Header.css';
-
+import Logo from '../UI/Logo/Logo';
 
 const Header = () => {
     return (
         <header className='main-header'>
+            <Logo />
             <Nav />            
         </header>
     )
