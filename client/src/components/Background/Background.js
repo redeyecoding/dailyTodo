ad244696eaeba03235efd9d795abed889ce85ebb
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-02 19:46:18 -0500

    added image background and background color

---
+++ client/src/components/Background/Background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Background.css';
+
+const Background = () => (
+    <>
+        <div className="background-color positioning"></div> 
+        <div className="log-reg__background positioning"></div>
+        
+    </>
+    
+    
+    );
+
+export default Background;
