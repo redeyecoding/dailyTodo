ea66753dfdea6277e9d9b0834e8615d162280691
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-05 15:47:45 -0500

    added code to switch between logged in header and logged out header.. past down props

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthNav from '../UI/Nav/AuthNav';
 import Card from '../UI/Card/Card';
 import classes from './Header.css';
 import Logo from '../UI/Logo/Logo';
 
 const Header = () => {
+    const isLoggedIn = true;
     return (
         <header className='main-header'>
-            <Logo />
-            <AuthNav />            
+            <Logo
+                isLoggedIn={ isLoggedIn } />
+            <AuthNav
+                isLoggedIn={ isLoggedIn }
+              />
         </header>
     )
 };
