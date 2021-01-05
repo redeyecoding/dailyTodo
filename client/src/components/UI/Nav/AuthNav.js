b7a34746ec9f9ad13fe9a5f50d41bc85178d48c6
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-05 08:32:24 -0500

    rename Nav component to AuthNav

---
+++ client/src/components/UI/Nav/AuthNav.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './AuthNav.css';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../UI/Logo/Logo';
+
+const AuthNav = props => {
+    // Right code for having two separate headers.
+    return (
+        <>           
+            <nav className="head-main-menu header-area">
+                <ul className="head-main-menu_items">
+                    <li className="head-main-menu_label">
+                        <NavLink
+                            className="nav-link"
+                            to="/">Home</ NavLink>
+                    </li>
+
+                    <li className="head-main-menu_label">
+                        <NavLink 
+                            className="nav-link"
+                            to="/profile">Profile</ NavLink>
+                    </li>
+
+                    <li className="head-main-menu_label">
+                        <NavLink 
+                            className="nav-link user-lists"
+                            to="/my-lists">My Lists</ NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
+
+    )
+};
+
+export default AuthNav;
