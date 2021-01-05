c9bcad2f12c17d165c3338e9687c71f17f0a19f5
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-05 15:48:54 -0500

    added some code to switch between classes when user is logged in and out

--- client/src/components/UI/Nav/AuthNav.js
+++ client/src/components/UI/Nav/AuthNav.js
@@ -5,29 +5,35 @@ import Logo from '../../UI/Logo/Logo';
 
 const AuthNav = props => {
     // Right code for having two separate headers.
+    const isLoggedIn = props.isLoggedIn;
+
     return (
-        <>           
-            <nav className="head-main-menu header-area">
-                <ul className="head-main-menu_items">
-                    <li className="head-main-menu_label">
-                        <NavLink
-                            className="nav-link"
-                            to="/">Home</ NavLink>
-                    </li>
+        <>   
+            { 
+                props.isLoggedIn &&        
 
-                    <li className="head-main-menu_label">
-                        <NavLink 
-                            className="nav-link"
-                            to="/profile">Profile</ NavLink>
-                    </li>
+                <nav className="head-main-menu header-area">
+                    <ul className="head-main-menu_items">
+                        <li className="head-main-menu_label">
+                            <NavLink
+                                className="nav-link"
+                                to="/">Home</ NavLink>
+                        </li>
 
-                    <li className="head-main-menu_label">
-                        <NavLink 
-                            className="nav-link user-lists my-lists"
-                            to="/my-lists">My Lists</ NavLink>
-                    </li>
-                </ul>
-            </nav>
+                        <li className="head-main-menu_label">
+                            <NavLink 
+                                className="nav-link"
+                                to="/profile">Profile</ NavLink>
+                        </li>
+
+                        <li className="head-main-menu_label">
+                            <NavLink 
+                                className="nav-link user-lists my-lists"
+                                to="/my-lists">My Lists</ NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            }
         </>
 
     )
