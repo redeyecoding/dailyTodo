914723c73697d1f2dff7818aacdba21c1b3db1b9
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-06 16:00:28 -0500

    rename login and register background

---
+++ client/src/components/Background/AuthBackground.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AuthBackground.css';
+
+const Background = () => (
+    <>
+        <div className="background-color positioning"></div> 
+        <div className="log-reg__background positioning"></div>        
+    </>
+    
+    
+    );
+
+export default Background;
