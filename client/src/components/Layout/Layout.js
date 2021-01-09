6d06d915292bacd24810cbd8cdfdfe883eeafbb5
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-08 22:21:21 -0500

    removed footer

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Auth from '../../components/Auth/Auth';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 
 const Layout = () => {
     return (
         <>
             <Header />
             <Auth />
-            <Footer />
         </>
     )    
 };
