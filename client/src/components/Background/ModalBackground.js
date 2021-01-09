ad1d09eb4eee4a9c0c06c711488c8641a4742189
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-09 12:34:17 -0500

    testing Background Modal

---
+++ client/src/components/Background/ModalBackground.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../UI/Card/Card';
+import './ModalBackground.css';
+
+const ModalBackground = props => {
+    return (
+
+        <div className='ModalBack'></div>
+    )
+};
+
+export default ModalBackground;
