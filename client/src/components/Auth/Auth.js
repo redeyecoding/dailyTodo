b3a0c43bb016a6596b535a376ad881aa924c34b1
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-10 15:38:00 -0500

    modified code to work with new Modal

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -3,25 +3,18 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 
 const Auth = () => {
-
     const [ toggleState, setToggleState ] = useState(false);
-    console.log('STATE')
     const formToggleHandler = () => {
-        console.log(toggleState)
         setToggleState(!toggleState)
     };
-     
-
     return (
             toggleState 
             ?
             <Register toggleForm={ formToggleHandler } />
             :
             <Login toggleForm={ formToggleHandler } />
-
-              
    )
-}
+};
 
 export default Auth;
 
