b3783f04e2f0137587ce20bca022ccf08b0a6ecd
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-11 22:01:27 -0500

    Added code to close error

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import Card from '../UI/Card/Card';
 import classes from './Modal.module.css';
 import { connect } from 'react-redux';
+import {
+    closeAlert
+ } from '../../store/actions/alert';
 
 
 const Modal = props => {
     return (
         props.isError 
         && 
-        <div className={ classes.Default_Modal }>
+        <div 
+            onClick={ () => props.onCloseAlert(false) }
+            className={ classes.Default_Modal }
+        >
             <span className={ classes.close_error }>X</span>
             <div className={ classes.error_message}>
                 <span>{ props.err }</span>
@@ -24,5 +29,12 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps)(Modal);
+const mapDispatchToProps = dispatch => {
+    return {
+        onCloseAlert: closeError => dispatch( closeAlert( closeError ))
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
 
