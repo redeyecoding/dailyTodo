6d9d2b6a2c547b9908e07e0380cb549c43e053fa
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-15 16:13:48 -0500

    added thankyou page

---
+++ client/src/components/Auth/ThankYou/ThankYou.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Thankyou = props => {
+    return (
+        <div>
+            <span>THANKS FOR REGISTERING!</span>
+        </div>
+    )
+};
+
+export default Thankyou;
