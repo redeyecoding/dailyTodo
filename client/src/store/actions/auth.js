46d80a3d5b73e384c162fbdcb3c7c302a1efcfe6
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-20 20:04:21 -0500

    added some code to test express-sessions

---
+++ client/src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import {
+    REGISTER_FAIL,
+    REGISTER_SUCCESS
+} from './actionTypes';
+
+export const login = ({ email, password }) => async dispatch => {
+    const body = JSON.stringify({ email, password });
+    
+    const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+    }; 
+
+    try {
+        const res = await axios.post('api/auth/', body, config)
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: 'BLAH'
+        });        
+    } catch {
+        // const errors = err.response.data.errors.map( errMessage => {
+        //     if ( typeof(errMessage)  === 'string' ) return errMessage;
+        //     return errMessage.msg;
+        // });
+        // console.error(errors);
+
+        dispatch({
+            type: REGISTER_FAIL,
+            payload: 'BLAH'
+        });   
+        // props.onAlert(errors.join(' '));
+    }
+
+    
+    // Testing cookies
+    // const res = await axios
+    // .get('api/auth/')
+    // .then( response => response) 
+    
+
+}
+
+
+
